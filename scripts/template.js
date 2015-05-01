04eef6676cdabfbc7880c7ef840f94358ea22c8c
Sophal Kim <sophal.kim.work@gmail.com>
2015-05-01 14:38:34 -0700

    Cool, added random fixed button

--- scripts/template.js
+++ scripts/template.js
@@ -2,3 +2,10 @@ function changeWidth() {
 	var column1 = document.getElementById('column1');
 	column1.style.width = "50%";
 }
+
+function randomMove() {
+	var button1 = document.getElementById('x');
+	button1.style.position = "fixed";
+	button1.style.top = Math.random() * 800 + 'px';
+	button1.style.left = Math.random() * 1500 + 'px';
+}
